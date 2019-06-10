c37230fcf13b3d3ce464ffd4003c844a5a52124b
Ameliza <36745160+Ameliza@users.noreply.github.com>
2019-06-10 20:48:59 -0300

    Atualizando filtro1

--- filtro1.m
+++ filtro1.m
@@ -17,29 +17,15 @@ thetas = f2/(fa/2);
 lambdap = 2*tan((thetap*pi)/2);
 lambdas = 2*tan((thetas*pi)/2);
 
-%ohmegas = lambdas/lambdap;
-Ws = lambdas/lambdap;
-Wp = 1;
+E = sqrt(10^(0.1*Ap)-1);
+n = ceil((log(10^(0.1*As)-1)/E^2)/(2*log(Ws))); % ordem do filtro
+k = 1:n;
+pk = E^(-1/n)*exp((1j*(2*k+n-1)/(2*n)*pi));
 
-%E = sqrt(10^(0.1*Ap)-1);
-%n = ceil((log(10^(0.1*As)-1)/E^2)/(2*log(Ws))); % ordem do filtro
-%k = 1:n;
-%pk = E^(-1/n)*exp((1j*(2*k+n-1)/(2*n)*pi));
-
-[n,Wn] = buttord(Wp,Ws,Ap,As,'s');
-[b,a] = butter(n,Wn,'s');
+% CALCULAR H(P), ACHANDO PRIMEIRO O P E O D(P)
 
 syms p;
-Np(p) = poly2sym(b, p);
-Dp(p) = poly2sym(a, p);
-Hp(p) = Np(p) / Dp(p);
-
-pretty(vpa(collect(Hp(p)), 5));
-
-syms s;
-Hs(s) = collect(subs(Hp(p), s/Wp));
-[N, D] = numden(Hs(s));
-pretty(vpa(Hs(s), 3));
+%H(p) = 
 
 % -------------------- Plotando --------------------
 figure(1)
@@ -51,3 +37,14 @@ semilogx(w, 20*log10(abs(h)))
 ylim([-80 10])
 grid on
 hold off
+
+
+%% Projeto Filtro FIR - Janela Ajustável
+
+Ap = 2; % Ganho na banda de passagem em dB
+As = 30; % Atenuação no stopband em dB
+fa = 4000; % Hz
+f1 = 1000; % Hz
+f2 = 1300; % Hz
+GdB = 5; % dB
+
